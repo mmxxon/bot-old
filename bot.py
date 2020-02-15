9357718d7d81e8cdac4c15b2fa89f37c5a49535b
xon <makson.vs.code@gmail.com>
2020-02-15 13:45:43 +0200

    banlog

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import telebot
 import os
 from pymongo import MongoClient
-import utils
-from utils import TOKEN, uri, extract_arg, heroku_check, kat
+import utils_global
+from utils_global import TOKEN, uri, extract_arg, heroku_check, kat
 
 myclient = MongoClient(uri)
 mydb = myclient["userdb"]
@@ -14,7 +14,7 @@ bot = telebot.AsyncTeleBot(TOKEN)
 @bot.message_handler(commands=["start"])
 def start(message):
     if message.chat.type == "private":
-        utils.log(message)
+        utils_global.log(message)
         is_user = users.find_one({"_id": message.from_user.id})
         if str(is_user) == "None":
             users.insert_one(
@@ -26,8 +26,8 @@ def start(message):
                     "small": 0,
                 }
             )
-            key = utils.small(1)
-            bot.reply_to(message, utils.txtstart, reply_markup=key)
+            key = utils_global.small(1)
+            bot.reply_to(message, utils_global.txtstart, reply_markup=key)
         else:
             if is_user["ban"] == 0:
                 if is_user["username"] != message.from_user.username:
@@ -41,9 +41,9 @@ def start(message):
                         {"$set": {"username": message.from_user.first_name}},
                     )
                 if is_user["small"] == 0:
-                    data = utils.small(1)
+                    data = utils_global.small(1)
                 else:
-                    data = utils.small(0)
+                    data = utils_global.small(0)
                 bot.reply_to(
                     message,
                     "Вы уже подписаны на обновления бота",
@@ -53,9 +53,47 @@ def start(message):
         bot.reply_to(message, "Works only in private chats")
 
 
+@bot.message_handler(commands=["papuga"])
+def ppuga(message):
+    if message.chat.type == "private":
+        utils_global.log(message)
+        for i in papug.aggregate([{"$sample": {"size": 1}}]):
+            id = i["_id"]
+            try:
+                bot.send_photo(message.chat.id, id)
+            except:
+                continue
+    else:
+        bot.reply_to(message, "Works only in private chats")
+
+
+"""
+@bot.message_handler(commands=["minesweeper"])
+def size_menu1(message):
+    findlist = ulist.find_one({"_id": message.from_user.id})
+    if str(findlist) == "None" and str(message.from_user.id) != bot_id:
+
+"""
+
+
+@bot.callback_query_handler(lambda query: "small" in query.data)
+def smallq(call):
+    val = int(call.data[5])
+    users.update_one({"_id": call.from_user.id}, {"$set": {"small": val}})
+    if val == 1:
+        text = "Вы подписались на новости о небольших фиксах"
+    else:
+        text = "Вы отписались от новостей о небольших фиксах"
+    bot.answer_callback_query(
+        callback_query_id=call.id, text=text, show_alert=1
+    )
+
+
+# ADMIN
 @bot.message_handler(commands=["ban"])
 def ban(message):
-    if str(message.from_user.id) == utils.admin_id:
+    utils_global.log(message)
+    if str(message.from_user.id) == utils_global.admin_id:
         argument = extract_arg(message.text)
         for i in argument:
             usr = int(i)
@@ -88,7 +126,8 @@ def ban(message):
 
 @bot.message_handler(commands=["unban"])
 def unban(message):
-    if str(message.from_user.id) == utils.admin_id:
+    utils_global.log(message)
+    if str(message.from_user.id) == utils_global.admin_id:
         argument = extract_arg(message.text)
         for i in argument:
             usr = int(i)
@@ -121,7 +160,7 @@ def unban(message):
 
 @bot.message_handler(commands=["mass"])
 def mass(message):
-    if str(message.from_user.id) == utils.admin_id:
+    if str(message.from_user.id) == utils_global.admin_id:
         argument = extract_arg(message.text)
         txt = " ".join(argument)
         cursor = users.find({})
@@ -135,7 +174,7 @@ def mass(message):
 
 @bot.message_handler(commands=["massmall"])
 def massmall(message):
-    if str(message.from_user.id) == utils.admin_id:
+    if str(message.from_user.id) == utils_global.admin_id:
         argument = extract_arg(message.text)
         txt = " ".join(argument)
         cursor = users.find({})
@@ -151,7 +190,7 @@ def massmall(message):
 @bot.message_handler(content_types=["photo"])
 def papuga(message):
     if (
-        str(message.from_user.id) == utils.admin_id
+        str(message.from_user.id) == utils_global.admin_id
         or str(message.from_user.id) == kat
     ):
         find = papug.find_one({"_id": message.photo[-1].file_id})
@@ -164,31 +203,9 @@ def papuga(message):
             papug.insert_one({"_id": message.photo[-1].file_id})
 
 
-@bot.message_handler(commands=["papuga"])
-def ppuga(message):
-    if message.chat.type == "private":
-        utils.log(message)
-        for i in papug.aggregate([{"$sample": {"size": 1}}]):
-            id = i["_id"]
-            try:
-                bot.send_photo(message.chat.id, id)
-            except:
-                continue
-    else:
-        bot.reply_to(message, "Works only in private chats")
-
-
-@bot.callback_query_handler(lambda query: "small" in query.data)
-def smallq(call):
-    val = int(call.data[5])
-    users.update_one({"_id": call.from_user.id}, {"$set": {"small": val}})
-    if val == 1:
-        text = "Вы подписались на новости о небольших фиксах"
-    else:
-        text = "Вы отписались от новостей о небольших фиксах"
-    bot.answer_callback_query(
-        callback_query_id=call.id, text=text, show_alert=1
-    )
+#
+#
+#
 
 
 if heroku_check():
@@ -210,7 +227,7 @@ if heroku_check():
     @server.route("/")
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url=str(utils.url) + TOKEN)
+        bot.set_webhook(url=str(utils_global.url) + TOKEN)
         return "!", 200
 
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
